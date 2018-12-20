da4be8b0134afc2acf5100fb527b504321fb641d
markus <markusvanlaak@gmail.com>
2018-12-20 11:30:51 +0100

    bad code

--- main.go
+++ main.go
@@ -1,64 +1,70 @@
-package bad
+package main
 
-const (
-	AdexpType = 0
-	IcaoType  = 1
+import (
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-const upperLevel = 350
+func timeConversion(s string) string {
+	/*
+	 * Write your code here.
+	 */
+	fmt.Println(s)
 
-type estdata struct {
-	Ptid        string
-	Eto         string
-	FlightLevel int
-}
+	if strings.HasSuffix(s, "PM") {
+		s = strings.TrimSuffix(s, "PM")
+		var hourint int
+		ssplitted := strings.Split(s, ":")
 
-type geo struct {
-	Geoid     string
-	Latitude  string
-	Longitude string
-}
-
-type rtepts struct {
-	Ptid        string
-	FlightLevel int
-	Eto         string
-}
-
-type Message struct {
-	Type        int
-	Title       string
-	Adep        string
-	Ades        string
-	Alternate   string
-	Arcid       string
-	ArcType     string
-	Ceqpt       string
-	MessageText string
-	Comment     string
-	Eetfir      []string
-	Speed       []string
-	Estdata     []estdata
-	Geo         []geo
-	RoutePoints []rtepts
-}
-
-type simpleToken struct {
-	token string
-	value string
-}
-
-type complexToken struct {
-	token string
-	value []map[string]string
-}
-
-func IsUpperLevel(m Message) bool {
-	for _, r := range m.RoutePoints {
-		if r.FlightLevel > upperLevel {
-			return true
+		switch hour := ssplitted[0]; hour {
+		case "01":
+			hourint = 1
+		case "02":
+			hourint = 2
+		case "03":
+			hourint = 3
+		case "04":
+			hourint = 4
+		case "05":
+			hourint = 5
+		case "06":
+			hourint = 6
+		case "07":
+			hourint = 7
+		case "08":
+			hourint = 8
+		case "09":
+			hourint = 9
+		case "10":
+			hourint = 10
+		case "11":
+			hourint = 11
+		case "12":
+			ssplitted[0] = "12"
+			hourint = 12
 		}
+
+		if hourint < 12 {
+			ssplitted[0] = strconv.Itoa(hourint + 12)
+		}
+
+		s = ssplitted[0] + ":" + ssplitted[1] + ":" + ssplitted[2]
 	}
 
-	return false
+	if strings.HasSuffix(s, "AM") {
+		s = strings.TrimSuffix(s, "AM")
+		ssplitted := strings.Split(s, ":")
+		if strings.Contains(ssplitted[0], "12") {
+			ssplitted[0] = "00"
+		}
+		s = ssplitted[0] + ":" + ssplitted[1] + ":" + ssplitted[2]
+	}
+	return s
+}
+
+func main() {
+	s := "12:45:54PM"
+	a := timeConversion(s)
+	fmt.Println(a)
 }
